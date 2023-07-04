a53ebd5b881bcd0e09b14b65030afb143d90e3ec
Georgiy Zubets <gzubecz@bk.ru>
2023-07-04 17:51:24 +0300

    add cases article

---
+++ js/script.js
@@ -0,0 +1,14 @@
+window.addEventListener("DOMContentLoaded", function () {
+    const navMenuButton = document.querySelector("#nav__menu__open");
+    navMenuButton.addEventListener("click", function () {
+        const menu = document.querySelector(".menu__mobile");
+        menu.style.display = "";
+    });
+    window.addEventListener("click", function (event) {
+        if (event.target.closest("#nav__menu__open")) {
+            return;
+        }
+        const menu = document.querySelector(".menu__mobile");
+        menu.style.display = "none";
+    });
+});
